6b1060cd71e73bbad95e837bf738aa893c7e1ce1
Meining <dora@Meinings-Air.ht.home>
2023-09-21 00:17:48 -0400

    Activity 3 add timestamp

--- hello.py
+++ hello.py
@@ -1,16 +1,36 @@
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+from datatime import datatime
 
 app = Flask(_name_)
 bootstrap = Bootstrap(app)
+moment = Moment(app)
 
 #'<h1>Hello World!</h1>'
 #'<h1>Hello, {}!</h1>'.format(name)
-
+'''
 @app.route('/')
 def index():
     return render_template('index.html')
+'''
+@app.route('/')
+def index():
+    return '<h1>Hello Meg!</h1>'
+
 
 @app.route('/user/<name>')
 def user(name):
     return render_template('user.html', name = name)
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@app.route('/')
+def index():
+    return render_template('index.html', current_time=datetime.utcnow()) 
