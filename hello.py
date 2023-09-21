31521b429634b65dc263cfee22117e6100eb16b1
Meining <dora@Meinings-Air.ht.home>
2023-09-20 21:55:50 -0400

    Fixed typo in example 2-2

--- hello.py
+++ hello.py
@@ -5,6 +5,6 @@ app = Flask(_name_)
 def index():
     return '<h1>Hello World!</h1>'
 
-@app.route('/user/<name')
+@app.route('/user/<name>')
 def user(name):
     return '<h1>Hello, {}!</h1>'.formate(name)
